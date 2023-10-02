05d6022271173ebf8d5544065e9a29033b6db5e0
Aheadz <zahead.rashidi@student.uts.edu.au>
2023-10-02 22:02:40 +1100

    disabled api monitoring, to send command manually

--- OctoprintMonitor.py
+++ OctoprintMonitor.py
@@ -1,7 +1,8 @@
 import time
 import requests
 import rospy
-from std_msgs.msg import Int32
+from std_msgs.msg import Bool
+from std_msgs.msg import Int8
 
 # Replace with your OctoPrint API key
 API_KEY = 'your_api_key'
@@ -11,6 +12,8 @@ OCTOPRINT_ADDRESS = 'http://your_octoprint_address/api/job'
 
 # Set the polling interval (in seconds)
 POLLING_INTERVAL = 60  # 60 seconds
+
+printer_id = 0
 
 def get_print_status():
     headers = {
@@ -22,26 +25,33 @@ def get_print_status():
 
 def wait_for_print_to_complete():
     while True:
-        status = get_print_status()
-        if status == 'Operational':
+        #status = get_print_status()
+        #if status == 'Operational':
+        #    break
+        print("Printer To Remove")
+        global printer_id
+        printer_id = int(input('--->'))
+        if printer_id != 0:
             break
         time.sleep(POLLING_INTERVAL)
 
 def publish_message():
     # Initialize the ROS node
     rospy.init_node('octoprint_notifier')
-
     # Create a Publisher object
-    pub = rospy.Publisher('octoprint_status', Int32, queue_size=10)
-
+    pub_status = rospy.Publisher('/octoprint_status', Bool, queue_size=10)
+    pub_id = rospy.Publisher('/octoprint_finished_printers' ,Int8,queue_size=10)
     # Wait for the print to complete
     wait_for_print_to_complete()
-
     # Publish the message
-    msg = Int32()
-    msg.data = 1  # you can use any integer value to signify that the print is complete
-    pub.publish(msg)
-
+    msg = Bool()
+    msg.data = True
+    pub_status.publish(msg)
+    
+    msg = Int8()
+    msg.data = printer_id
+    pub_id.publish(msg)
+    
     # Keep the script alive until the node is manually terminated
     rospy.spin()
 
